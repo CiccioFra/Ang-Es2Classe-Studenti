49b3ffc6b18812a6d3165350e09ff3e305eae622
Francesco <francesco.ribatti@engimtorino.net>
2022-03-28 11:30:51 +0200

    Creazione Progetto

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddStudentComponent,
+    StudentListComponent,
+    EditStudentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
